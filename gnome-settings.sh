1f082cb8bbb82b61ba92f54f3b636706bc014b47
Gustav Johansson <gustavsj@outlook.com>
2021-07-18 13:59:12 +0200

    gnome-settings: remove animation

--- gnome-settings.sh
+++ gnome-settings.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-gsettings set org.gnome.desktop.wm.preferences button-layout ":close,minimize,maximize"
+gsettings set org.gnome.desktop.wm.preferences button-layout ":close"
 gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface enable-animations false
