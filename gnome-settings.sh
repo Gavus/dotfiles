3b025021430cff837d891c5903c806780b1e9fc2
Gustav Johansson <gustavsj@outlook.com>
2021-09-03 08:54:37 +0200

    Update gnome settings

--- gnome-settings.sh
+++ gnome-settings.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gsettings set org.gnome.desktop.wm.preferences button-layout ":close"
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.desktop.interface clock-show-seconds true
@@ -8,4 +8,4 @@ gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface enable-animations false
-gsettings set org.gnome.desktop.interface scaling-factor 1
+#gsettings set org.gnome.desktop.interface scaling-factor 2
