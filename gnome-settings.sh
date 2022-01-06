b347926b49651bd31ae792a4880dd30cd73d231d
Gustav Johansson <gustavsj@outlook.com>
2022-01-06 11:48:29 +0100

    gnome-settings: Update
    Change-Id: I6cc7cd61b7835ae8d688ba91dc9af6c9f2187b69

--- gnome-settings.sh
+++ gnome-settings.sh
@@ -1,13 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
+# misc
 gsettings set org.gnome.desktop.wm.preferences button-layout ":close"
-gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
 gsettings set org.gnome.desktop.calendar show-weekdate true
+gsettings set org.gnome.desktop.session idle-delay 0
+#gsettings set org.gnome.desktop.interface scaling-factor 2
+# set capslock to escape
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+
+# interface
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface enable-animations false
-#gsettings set org.gnome.desktop.interface scaling-factor 2
-gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 
+# touchpad
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
+gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
