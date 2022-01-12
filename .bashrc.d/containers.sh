4badf1de06d584b33dd2bec9bd8aa5ddf5a44e2c
Gustav Johansson <gustavsj@outlook.com>
2022-01-12 13:41:45 +0100

    containers.sh: Fix docker alias bug

--- .bashrc.d/containers.sh
+++ .bashrc.d/containers.sh
@@ -1,23 +1,27 @@
 #!/bin/bash -e
 
+docker=docker
+
 alias t="toolbox"
 alias te="toolbox enter"
 alias tc="toolbox create"
 alias tl="toolbox list"
 
+if test -f "$(which podman)"; then
+	docker=podman
+fi
+
+if test -f /run/.containerenv; then
+	docker="flatpak-spawn --host podman"
+	podman="flatpak-spawn --host podman"
+fi
+
 de() {
 	if test "$#" -ne 1; then
 		echo "de <container-name/container-id>"
 	else
-		docker exec -it $1 bash
+		$docker exec -it $1 bash
 	fi
 }
 
-if test "$(which podman 2>/dev/null)"; then
-	alias docker="podman"
-fi
-
-if test -f /run/.containerenv; then
-	alias docker="flatpak-spawn --host podman"
-	alias podman="flatpak-spawn --host podman"
-fi
+alias docker=$docker
