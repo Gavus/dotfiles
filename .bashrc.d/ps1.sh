ef5481dd80a13535148f6d5aeac5467f61dcef6e
Gustav Johansson <gustavsj@outlook.com>
2022-01-17 22:19:40 +0100

    Change ps1.sh

--- .bashrc.d/ps1.sh
+++ .bashrc.d/ps1.sh
@@ -11,9 +11,11 @@ fi
 parse_git_branch() {
 	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
-GREEN="\e[0;32m"
-RED="\e[0;31m"
+
 BROWN="\e[0;33m"
+GREEN="\e[0;32m"
+BLUE="\e[1;34m"
+RED="\e[0;31m"
 STOP_COLOR="\e[m"
 
-export PS1="${GREEN}\u@${TOOLBOX_NAME} ${BROWN}\w ${RED}\$(parse_git_branch)${STOP_COLOR}\n\$ "
+export PS1="${BLUE}\u ${STOP_COLOR}at ${GREEN}${TOOLBOX_NAME} ${STOP_COLOR}in ${BROWN}\w ${STOP_COLOR}on ${RED}\$(parse_git_branch)${STOP_COLOR}\n\$ "
