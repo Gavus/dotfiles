cdf9dfc906d199d8b7c893c0f3176a9a5adc63fa
Gustav Johansson <gustavsj@outlook.com>
2022-03-07 09:47:25 +0100

    aliases: Add vg and va

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -4,3 +4,13 @@ alias vim="nvim"
 alias g="git"
 alias l="ls -lah"
 alias vimwiki="vim -c VimwikiIndex"
+
+# Open nvim where git detected changes.
+vg() {
+	nvim +"Git difftool $*"
+}
+
+# Open nvim and ag search.
+va() {
+	nvim +"Ag $*"
+}
