808a880523dbc19e298b1f6bc8b11dd7e0ee9e25
Gustav Johansson <gustavsj@outlook.com>
2022-03-14 11:24:11 +0100

    fv.sh: forward arguments to nvim

--- .bashrc.d/fv.sh
+++ .bashrc.d/fv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 fv () {
-	if test "$#" -ne 1; then
+	if test "$#" -lt 1; then
 		echo "Searches for a file and opens it with nvim"
-		echo "fv <file-to-search-for>"
+		echo "fv <file-to-search-for> <nvim options>"
 	else
-		nvim $(find . -wholename "*$1*" -print | head -n 1)
+		nvim $(find . -wholename "*$1*" -print | head -n 1) ${@:2}
 	fi
 }
 
