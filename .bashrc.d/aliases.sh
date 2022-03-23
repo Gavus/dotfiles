00cdaf57935d23fc640506eb6d54bfe8015831fc
Gustav Johansson <gustavsj@outlook.com>
2022-03-23 08:55:47 +0100

    bashrc.d Update sources

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,17 +1,23 @@
 #!/bin/bash -e
 
-alias vim="nvim"
 alias g="git"
 alias gs="echo 'Did you mean g s?'; g s"
 alias l="ls -lah"
+alias t="toolbox"
+alias tc="toolbox create"
+alias te="toolbox enter"
+alias tl="toolbox list"
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
 alias vimwiki="vim -c VimwikiIndex"
 
 # Open nvim where git detected changes.
 vg() {
-	nvim +"Git difftool $*"
+	nvim +"Git difftool $@"
 }
 
 # Open nvim and ag search.
 va() {
-	nvim +"Ag $*"
+	nvim +"Ag $@"
 }
--- .bashrc.d/flatpak-spawn.sh
+++ .bashrc.d/flatpak-spawn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 host() {
-    h $@
+    flatpak-spawn --host $@
 }
