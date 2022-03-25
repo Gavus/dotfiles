18a85efd7f4d77243a0518de8c698e0f5ed4c904
Gustav Johansson <gustavsj@outlook.com>
2022-03-25 13:35:53 +0100

    fv.sh: Use -name instead of -wholename

--- .bashrc.d/fv.sh
+++ .bashrc.d/fv.sh
@@ -5,7 +5,7 @@ fv () {
 		echo "Searches for a file and opens it with nvim"
 		echo "fv <file-to-search-for> <nvim options>"
 	else
-		nvim $(find . -wholename "*$1*" -print | head -n 1) ${@:2}
+		nvim $(find . -name $1 -print | head -n 1) ${@:2}
 	fi
 }
 
