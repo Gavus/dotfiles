33aaf23ec89066a0f8bfdc6b63bbf8b69dbc3e54
Gustav Johansson <gustavsj@outlook.com>
2022-04-08 18:01:17 +0200

    Add install-go.sh

---
+++ install-go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+version=1.18
+installpath=~/.local/share/go${version}
+tarfile=go${version}.linux-amd64.tar.gz
+
+if test -d "$installpath"; then
+	echo "go${version} is already installed"
+else
+	wget https://go.dev/dl/$tarfile
+	tar -xzvf ./$tarfile
+	rm $tarfile
+	mv go $installpath
+	ln -srf $installpath/bin/* ~/.local/bin/
+fi
