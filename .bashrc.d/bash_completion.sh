77c0e1119d92cb4cf42a4583c510381416963235
Gustav Johansson <gustavsj@outlook.com>
2022-04-10 08:36:52 +0200

    bash_completion.sh: Add extra path

--- .bashrc.d/bash_completion.sh
+++ .bashrc.d/bash_completion.sh
@@ -2,5 +2,6 @@
 
 if test -f "/etc/profile.d/bash_completion.sh"; then
 	source "/etc/profile.d/bash_completion.sh"
+elif test -f "/usr/share/bash-completion/bash_completion"; then
+	source "/usr/share/bash-completion/bash_completion"
 fi
-
