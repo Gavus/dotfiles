ff2fc745d38c5a4ad3257119ef463c9aad4c9a88
Gustav Johansson <gustavsj@outlook.com>
2022-06-05 09:14:03 +0200

    download-font: auto install fonts

--- download-font.sh
+++ download-font.sh
@@ -6,6 +6,16 @@ if test ! -f SourceCodePro.zip; then
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip
 fi
 unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf > linux.ttf
+unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Medium\ Nerd\ Font\ Complete.ttf > linux-medium.ttf
+unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Bold\ Nerd\ Font\ Complete.ttf > linux-bold.ttf
 unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Nerd\ Font\ Complete\ Windows\ Compatible.ttf > windows.ttf
+rm SourceCodePro.zip
 
-echo "Fonts downloaded. Remove 'fonts' directory when installed."
+mkdir -p ~/.fonts
+cp * ~/.fonts/
+fc-cache -fv | grep home
+fc-list | grep home
+
+cd -
+
+echo "Done."
