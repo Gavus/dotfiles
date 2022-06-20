af57d81b5cd61772085c6106ee2cd3999e162f82
Gustav Johansson <gustavsj@outlook.com>
2022-06-20 11:03:23 +0200

    Astronvim: Make cursor blink

--- astronvim/options.lua
+++ astronvim/options.lua
@@ -1,7 +1,7 @@
 return {
   opt = {
     termguicolors = true, -- Enables 24-bit RGB color in the TUI.
-    guicursor="a:blinkon10", -- Make cursor blink.
+    guicursor="n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkon10", -- Make cursor blink.
 
     -- Text
     wrap = true, -- Wrap text.
