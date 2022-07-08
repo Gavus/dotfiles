1a195e9d543af797974655285dab697d27c3cd3d
Gustav Johansson <gustavsj@outlook.com>
2022-07-08 15:53:31 +0200

    Astronvim: Add tabstop 4
    Make vimwiki create files with - instead of whitespace.

--- astronvim/options.lua
+++ astronvim/options.lua
@@ -8,9 +8,11 @@ return {
     linebreak = true, -- Break at space.
     list = true, -- Show certain characters.
     listchars="tab:▷▷", -- Show tabs.
+    tabstop=4 -- Tab is the same width as 4 whitespaces.
   },
   g = {
     vimwiki_list = {{path = '~/vimwiki', syntax = 'markdown', ext = '.md'}}, -- Make vimwiki use markdown.
     vimwiki_map_prefix = '<Leader>v', -- Change vimwiki leader key to space v.
+    vimwiki_links_space_char = '-', -- Make vimwiki create names with - instead of space.
   },
 }
