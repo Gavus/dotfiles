1c8fdcec48a66b39541d4e0f2901b81eb107a17b
Gustav Johansson <gustavsj@outlook.com>
2022-09-06 15:09:57 +0200

    Astronvim: Remove guicursor and wrap

--- astronvim/options.lua
+++ astronvim/options.lua
@@ -1,10 +1,8 @@
 return {
   opt = {
     termguicolors = true, -- Enables 24-bit RGB color in the TUI.
-    guicursor="n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkon10", -- Make cursor blink.
 
     -- Text
-    wrap = true, -- Wrap text.
     linebreak = true, -- Break at space.
     list = true, -- Show certain characters.
     listchars="tab:▷▷", -- Show tabs.
