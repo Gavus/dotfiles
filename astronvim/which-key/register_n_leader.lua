ef2c581d11570e04b136652fa518861e4188d925
Gustav Johansson <gustavsj@outlook.com>
2022-09-23 19:01:02 +0200

    Astronvim: Add ClangdSwitchSourceHeader binding leader+lH

--- astronvim/which-key/register_n_leader.lua
+++ astronvim/which-key/register_n_leader.lua
@@ -1,10 +1,11 @@
 return {
-  lh = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
-  lk = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
   lg = {
     name = "Goto",
     d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
     D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
   },
+  lh = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
+  lH = { "<cmd>ClangdSwitchSourceHeader<CR>", "Switch source/header" },
+  lk = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
   v = { name = "Vimwiki" },
 }
