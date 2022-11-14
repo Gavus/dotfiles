5e5697d716b9614b1e3c9951b7a83aece59146fc
Gustav Johansson <gustavsj@outlook.com>
2022-11-14 17:18:27 +0100

    Add gdbinit dashboard
    Move scripts to install-scripts.

--- install-scripts/configs.sh
+++ install-scripts/configs.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 
+toplevel="$(git rev-parse --show-toplevel)"
+
 echo "Creating syslinks"
-ln -srf .bashrc "$HOME/"
-ln -srf .bashrc.d "$HOME/"
-ln -srf .inputrc "$HOME/"
-ln -srf .tmux.conf "$HOME/"
+ln -srf "$toplevel/.bashrc" "$HOME/"
+ln -srf "$toplevel/.bashrc.d" "$HOME/"
+ln -srf "$toplevel/.inputrc" "$HOME/"
+ln -srf "$toplevel/.tmux.conf" "$HOME/"
+ln -srf "$toplevel/.gdbinit" "$HOME/"
 
 mkdir -p "$HOME/.local/bin"
-ln -srf "./bin/"* "$HOME/.local/bin/"
+ln -srf "$toplevel/bin/"* "$HOME/.local/bin/"
 
 if test ! -d "$HOME/.tmux/plugins/tpm"; then
 	echo "Cloning tmux plugin manager"
