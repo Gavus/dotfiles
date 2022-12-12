66d9095708aea9419c9c415055eb0d9a0238ee17
Gustav Johansson <gustavsj@outlook.com>
2022-12-12 14:31:00 +0100

    install-scripts: Add lazygit

---
+++ install-scripts/lazygit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version="0.36.0"
+installdir="$HOME/.local/bin"
+url="https://github.com/jesseduffield/lazygit/releases/download/v${version}/lazygit_${version}_Linux_32-bit.tar.gz"
+
+mkdir -p "$installdir"
+wget -q -O- "$url" | tar -xzv lazygit
+chmod +x lazygit
+mv lazygit "$installdir"
