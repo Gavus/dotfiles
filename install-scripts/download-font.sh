2b9a6afa91bcd8ae89796558c8234330d36fa3bf
Gustav Johansson <gustavsj@outlook.com>
2023-04-03 16:21:09 +0200

    install-scripts/download-font.sh: Change to mono

--- install-scripts/download-font.sh
+++ install-scripts/download-font.sh
@@ -5,14 +5,12 @@ cd fonts
 if test ! -f SourceCodePro.zip; then
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip
 fi
-unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf > linux.ttf
-unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Medium\ Nerd\ Font\ Complete.ttf > linux-medium.ttf
-unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Bold\ Nerd\ Font\ Complete.ttf > linux-bold.ttf
-unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Nerd\ Font\ Complete\ Windows\ Compatible.ttf > windows.ttf
+unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Bold\ Nerd\ Font\ Complete\ Mono.ttf > linux-bold.ttf
+unzip -np SourceCodePro.zip Sauce\ Code\ Pro\ Nerd\ Font\ Complete\ Mono\ Windows\ Compatible.ttf > windows.ttf
 rm SourceCodePro.zip
 
 mkdir -p ~/.fonts
-cp * ~/.fonts/
+cp ./* ~/.fonts/
 fc-cache -fv | grep home
 fc-list | grep home
 
