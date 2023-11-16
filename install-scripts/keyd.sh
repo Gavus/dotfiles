fc063c80599c2261080be631cd0e48bfb5c80bed
Gustav Johansson <gustavsj@outlook.com>
2023-11-16 12:44:59 +0100

    keyd: Add spacefn + shift = backslash

--- install-scripts/keyd.sh
+++ install-scripts/keyd.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 
-#Keyd is used to remap capslock into esc when tapped and control when held.
-
 git clone https://github.com/rvaiya/keyd --depth 1 -b v2.4.3
 bash -c "cd keyd && make && sudo make install" || true
-rm -rf keyd 
+rm -rf keyd
 sudo systemctl enable keyd
 sudo systemctl start keyd
 
