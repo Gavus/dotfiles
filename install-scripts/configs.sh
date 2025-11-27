2936d26a5ac32155ef1228b8cbde7e0f55159265
Gustav Johansson <gustajo@axis.com>
2025-11-27 16:57:00 +0100

    install configs: Use --remove-dest
    Change-Id: I1df8b8df97fa0af3182798a22e856f8d7a239e5b

--- install-scripts/configs.sh
+++ install-scripts/configs.sh
@@ -3,17 +3,17 @@
 toplevel="$(git rev-parse --show-toplevel)"
 
 mkdir -p "$HOME/.bashrc.d"
-cp -f "$toplevel/bashrc" "$HOME/.bashrc"
-cp -f "$toplevel/bashrc.d/"* "$HOME/.bashrc.d/"
-cp -f "$toplevel/inputrc" "$HOME/.inputrc"
-cp -f "$toplevel/tmux.conf" "$HOME/.tmux.conf"
+cp --remove-destination "$toplevel/bashrc" "$HOME/.bashrc"
+cp --remove-destination "$toplevel/bashrc.d/"* "$HOME/.bashrc.d/"
+cp --remove-destination "$toplevel/inputrc" "$HOME/.inputrc"
+cp --remove-destination "$toplevel/tmux.conf" "$HOME/.tmux.conf"
 
 mkdir -p "$HOME/.local/bin"
-cp -f --remove-destination "$toplevel/bin/"* "$HOME/.local/bin/"
+cp --remove-destination "$toplevel/bin/"* "$HOME/.local/bin/"
 
 if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
     echo "Cloning tmux plugin manager"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 fi
 
 echo "Done"
