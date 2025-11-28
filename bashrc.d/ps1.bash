a648aa239e07971a0b25f703c02bbe0721a1e8c0
Gustav Johansson <gustajo@axis.com>
2025-11-28 08:58:03 +0100

    PS1: effectivize by making it more static
    Change-Id: I04940765b3931f13381dced793601cd16c2be7b5

--- bashrc.d/ps1.bash
+++ bashrc.d/ps1.bash
@@ -2,42 +2,59 @@
 
 PROMPT_COMMAND=_prompt_command # Function to generate PS1 after CMDs.
 
+function _ps1_color() {
+    local bold=""
+    local color=""
+
+    color_code="$1"
+    bold="$(tput bold)"
+    color="$(tput setaf "$color_code")"
+    echo "${bold}${color}"
+}
+
+PS1_RED="$(_ps1_color 1)"
+PS1_GREEN="$(_ps1_color 2)"
+PS1_YELLOW="$(_ps1_color 3)"
+PS1_BLUE="$(_ps1_color 4)"
+PS1_PURPLE="$(_ps1_color 5)"
+PS1_CYAN="$(_ps1_color 6)"
+PS1_STOP_COLOR="$(tput sgr0)"
+PS1_CONTAINER=""
+PS1_USER="\u"
+PS1_DISTRO=""
+PS1_CONTAINER_ID=""
+PS1_HOSTNAME="\h"
+PS1_BRANCH=""
+PS1_WORKDIR=" \w"
+PS1_MACHINE=""
+PS1_NEWLINE="\n➤ "
+
+if [[ -f "/run/.containerenv" || -f "/.dockerenv" ]]; then
+    PS1_CONTAINER="🐋 "
+    PS1_CONTAINER_ID=" ${CONTAINER_ID}"
+fi
+
+if [[ -f "/etc/os-release" ]]; then
+    source /etc/os-release
+    PS1_DISTRO=" ${ID}-${VERSION_ID}"
+fi
+
+if [[ -f "$HOME/.bashrc.d/git-prompt.bash" ]]; then
+    source "$HOME/.bashrc.d/git-prompt.bash"
+    PS1_BRANCH='$(__git_ps1 "  %s ")'
+fi
+
 function _prompt_command() {
     local _exit="$?"
-    local yellow="\e[1;33m"
-    local green="\e[1;32m"
-    local blue="\e[1;34m"
-    local red="\e[1;31m"
-    local purple="\e[1;35m"
-    local cyan="\e[1;36m"
-    local stop_color="\e[m"
-    local container=""
-    local user="\u"
-    local distro=""
-    local container_id=""
-    local hostname="\h"
-    local branch=""
-    local workdir="\w"
-    local machine=""
-    local newline="\n➤ "
-    PS1=""
 
     if [[ ! -z "$MACHINE" ]]; then
-        machine="$MACHINE "
+        PS1_MACHINE=" $MACHINE"
     fi
 
-    if [[ -f "/run/.containerenv" ]] || [[ -f "/.dockerenv" ]]; then
-        container="🐋 "
-        container_id=" ${CONTAINER_ID}"
-    fi
-
-    source /etc/os-release
-    distro="${ID}-${VERSION_ID}"
-
-    source "$HOME/.bashrc.d/git-prompt.bash"
-    branch='$(__git_ps1 " %s ")'
-
-    PS1="${container}${purple}${user}@${hostname}${blue}${container_id} \
-${cyan}${distro} ${red}${machine}${green}${workdir} ${yellow}${branch}${red}${_exit} \
-${stop_color}${newline}"
+    PS1="${PS1_CONTAINER}\
+${PS1_PURPLE}${PS1_USER}@${PS1_HOSTNAME}\
+${PS1_BLUE}${PS1_CONTAINER_ID}${PS1_CYAN}${PS1_DISTRO}\
+${PS1_RED}${PS1_MACHINE}${PS1_GREEN}${PS1_WORKDIR}\
+${PS1_YELLOW}${PS1_BRANCH}${PS1_RED}${_exit}\
+${PS1_STOP_COLOR}${PS1_NEWLINE}"
 }
