761e28a9fa06267a36c9ede8541f76027b495d27
Gustav Johansson <gavus@me.com>
2026-01-11 17:03:07 +0100

    install-scripts/configs.sh: cleanup

--- install-scripts/configs.sh
+++ install-scripts/configs.sh
@@ -1,19 +1,29 @@
 #!/bin/bash -e
 
 toplevel="$(git rev-parse --show-toplevel)"
+bashdir="$HOME/.bashrc.d"
+bindir="$HOME/.local/bin"
+tpmdir="$HOME/.tmux/plugins/tpm"
+tpmurl="https://github.com/tmux-plugins/tpm"
 
-mkdir -p "$HOME/.bashrc.d"
+# Remove symlink if any.
+if [[ -L "$bashdir" ]]; then
+    rm "$bashdir"
+fi
+
+mkdir -p "$bashdir"
 cp --remove-destination "$toplevel/bashrc" "$HOME/.bashrc"
-cp --remove-destination "$toplevel/bashrc.d/"* "$HOME/.bashrc.d/"
+cp --remove-destination "$toplevel/bashrc.d/"* "$bashdir"
 cp --remove-destination "$toplevel/inputrc" "$HOME/.inputrc"
 cp --remove-destination "$toplevel/tmux.conf" "$HOME/.tmux.conf"
 
-mkdir -p "$HOME/.local/bin"
-cp --remove-destination "$toplevel/bin/"* "$HOME/.local/bin/"
+mkdir -p "$bindir"
+cp --remove-destination "$toplevel/bin/"* "$bindir"
 
-if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
+if [[ ! -d "$tpmdir" ]]; then
+    mkdir -p "$(dirname "$tpmdir")"
     echo "Cloning tmux plugin manager"
-    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+    git clone "$tpmurl" --depth=1 "$tpmdir"
 fi
 
 echo "Done"
