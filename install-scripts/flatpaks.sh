bb69acdd46d5764d255819b12442b81c62069a4d
Gustav Johansson <gavus@me.com>
2026-06-13 22:28:24 +0200

    install_scripts: Remove a bunch and update flatpaks

--- install-scripts/flatpaks.sh
+++ install-scripts/flatpaks.sh
@@ -1,31 +1,23 @@
 #!/bin/bash -e
 
-# shellcheck disable=2046
+FLATHUB_URL="https://flathub.org/repo/flathub.flatpakrepo"
 
-if test ! $(command -v flatpak); then
-    echo "Flatpak is not found, you have to install it manually!"
-    exit 1
+if [[ ! $(command -v flatpak) ]]; then
+  echo "Flatpak is not found, you have to install it manually!"
+  exit 1
 fi
 
-if test $(flatpak remote-list | grep -q flathub); then
-    echo "Adding flathub registry"
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo --user
+if ! flatpak remote-list | grep -q flathub; then
+  echo "Adding flathub registry"
+  flatpak remote-add --if-not-exists flathub $FLATHUB_URL --user
 fi
 
 flatpak install flathub --user \
-    com.discordapp.Discord \
-    com.microsoft.Teams \
-    com.mojang.Minecraft \
-    com.slack.Slack \
-    com.spotify.Client \
-    com.valvesoftware.Steam \
-    com.valvesoftware.Steam.CompatibilityTool.Proton \
-    com.valvesoftware.Steam.Utility.steamtinkerlaunch \
-    org.freedesktop.Platform.ffmpeg-full \
-    org.mozilla.firefox \
-    org.remmina.Remmina \
-    org.telegram.desktop \
-    org.videolan.VLC \
-    -y
+  com.github.tchx84.Flatseal \
+  com.vivaldi.Vivaldi \
+  org.kde.krita \
+  org.signal.Signal \
+  org.surge_synth_team.surge-xt \
+  -y
 
 echo "Done"
